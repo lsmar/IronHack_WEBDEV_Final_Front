00cb05ea7e0d037674c5318fd418a45806132dbf
LsM Lucas Martins <lucas.smar@gmail.com>
2019-10-25 12:18:57 -0300

    verify if the user was the role

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,28 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import SignUp from "../pages/SignUp";
+import Login from "../pages/Login";
 
 import { isAuthenticated } from "../services/auth";
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
+const PrivateRoute = ({ component: Component, role, ...rest }) => (
   <Route
     {...rest}
-    render={props =>
-      isAuthenticated() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
-      )
-    }
+    render={props => (isAuthenticated(role) ? <Component {...props} /> : <Redirect to={{ pathname: "/", state: { from: props.location } }} />)}
   />
 );
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={() => <h1>Login</h1>} />
+      <Route exact path="/" component={Login} />
       <Route path="/signup" component={SignUp} />
-      <PrivateRoute path="/app" component={() => <h1>App</h1>} />
+      <PrivateRoute path="/app" role="TEACHER" component={() => <h1>App</h1>} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   </BrowserRouter>
