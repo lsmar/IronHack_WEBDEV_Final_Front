cf8fcb03d2b6e1c1bd9e5b0b0846c192015dba02
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-25 07:28:59 -0300

    component input text

---
+++ src/components/input.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Input = ({ name, type, value, handleChange }) => (
+  <div className="">
+    <div className="">
+      <input
+        className=""
+        type={type}
+        placeholder={`enter ${name}`}
+        name={name}
+        value={value}
+        onChange={e => handleChange(e)}
+      />
+    </div>
+  </div>
+);
+
+export default Input;
