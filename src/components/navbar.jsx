53fa5631d247d9d404fa9c92f42b7b15e7d9b9ea
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-28 13:57:25 -0300

    navbar links added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 // SUBSTITUIR <a> POR <Link>
 // ADICIONAR IMAGENS DENTRO DOS <Link>
@@ -12,10 +13,10 @@ const Navbar = ({role}) => {
           <li><a href="https://google.com.br">1</a></li> :
           <li> <a href="https://google.com.br">PROFESSOR</a></li>
         } */}
-        <li> <a href="https://google.com.br"><img className='component-navbar-img' src="/images/Icons/Home.png" alt="home"/></a></li>
-        <li> <a href="https://google.com.br"><img className='component-navbar-img' src="/images/Icons/Conta.png" alt="Conta"/></a></li>
-        <li> <a href="https://google.com.br"><img className='component-navbar-img' src="/images/Icons/Professor.png" alt="+professor"/></a></li>
-        <li> <a href="https://google.com.br"><img className='component-navbar-img' src="/images/Icons/Projetos.png" alt="+projetos"/></a></li>
+        <li> <Link  to="/home"><img className='component-navbar-img' src="/images/Icons/Home.png" alt="home"/></Link></li>
+        <li> <Link  to="/home"><img className='component-navbar-img' src="/images/Icons/Conta.png" alt="Conta"/></Link></li>
+        <li> <Link  to="/newUser"><img className='component-navbar-img' src="/images/Icons/Professor.png" alt="+professor"/></Link></li>
+        <li> <Link  to="/newProject"><img className='component-navbar-img' src="/images/Icons/Projetos.png" alt="+projetos"/></Link></li>
       </ul>
     </nav>
   );
