192aca2690c84bcc2c339dd45726cdcf346fddd6
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-28 22:43:01 -0300

    delete page done

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,8 @@ import ProfessorCreated from "../pages/professorCreated";
 import RecordBookMainPage from "../pages/RecordBookMainPage";
 import RecordBookPerDate from "../pages/RecordBookPerDate";
 import RecordBookPerStudent from "../pages/RecordBookPerStudent";
+import EditProject from "../pages/editProject";
+import DeleteProject from "../pages/deleteProject";
 import Home from '../pages/Home'
 
 
@@ -40,6 +42,8 @@ const Routes = () => (
       <Route exact path="/project/:id/recordBook/:date" role="" component={RecordBookPerDate} />
       <Route exact path="/project/:id/recordBook/:date/:idRecord" role="" component={RecordBookPerStudent} />
       <Route exact path="/project/:id" role="" component={ProjectDetail} />
+      <Route exact exact path="/project/edit/:id" role="" component={EditProject} />
+      <Route exact exact path="/project/delete/:id" role="" component={DeleteProject} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   </BrowserRouter>
