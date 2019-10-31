cc5260f082e30dcd332827c0e303a3cddb09e33e
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-31 17:03:03 -0300

    loader on deleted page

--- src/pages/deleteProject.jsx
+++ src/pages/deleteProject.jsx
@@ -5,6 +5,7 @@ import apiAxios from "../services/api";
 import Logo from "../components/Logo";
 import Navbar from "../components/navbar";
 import TitleAndText from "../components/TitleAndText";
+import Loader from "../components/loader"
 
 class ProjectDeleted extends Component{
   constructor(props){
@@ -12,6 +13,7 @@ class ProjectDeleted extends Component{
     this.state={
       projectName: "",
       canDelete: false,
+      loader: true
     }
     this.deleteOne = this.deleteOne.bind(this);
     this.getOne = this.getOne.bind(this);
@@ -25,7 +27,7 @@ class ProjectDeleted extends Component{
     apiAxios
     .get(`/project/${this.props.match.params.id}`)
     .then(project => (
-      this.setState({projectName:project.data.name})
+      this.setState({projectName:project.data.name, loader: false})
     ))
     .catch(e => console.log(e));
   }
@@ -59,13 +61,14 @@ class ProjectDeleted extends Component{
 
       const messageConfirmation = (<div>
         <Logo />
-        <span className='page-delAndEdit-container'>
+        {this.state.loader !== true ? <span className='page-delAndEdit-container'>
         {this.state.projectName!==''?<TitleAndText>Apagar o projeto "{this.state.projectName}"</TitleAndText>:null}
         <Button type="submit" method={this.deleteOne} label={"Sim"} />
         <Link to="/project">
           <Button type="submit" label={"Nao, retornar para a home"} />
         </Link>
-        </span>
+        </span> : null}
+        {this.state.loader === true ? <Loader /> : null}
         <Navbar />
       </div>);
 
